8189e3e68cf76d7ca147c78a2bc7fb7e306855e6
Piyush <piyushnariani09@gmail.com>
2019-10-07 15:26:20 -0400

    Register Sync Task with SW

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -2,6 +2,9 @@ var shareImageButton = document.querySelector('#share-image-button');
 var createPostArea = document.querySelector('#create-post');
 var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
 var sharedMoments = document.querySelector('#shared-moments');
+var form = document.querySelector('form');
+var titleInput = document.querySelector('#title');
+var locationInput = document.querySelector('#location');
 
 function openCreatePostModal() {
   // createPostArea.style.display = 'block';
@@ -130,3 +133,19 @@ if('indexedDB' in window){
       }
     })
 }
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  if(titleInput.value.trim() === '' || locationInput.value.trim() === ''){
+    alert('Please enter valid data');
+    return;
+  }
+  closeCreatePostModal();
+
+  if('serviceWorker' in navigator && 'SyncManager' in window){
+    navigator.serviceWorker.ready
+      .then(function(sw) {
+        sw.sync.register('sync-new-post');
+      });
+  }
+})
